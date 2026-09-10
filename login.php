f233a084f5c6278a7f1dd4dd6499772f3a6170dd
muhamaddanielasyraf07 <muhamaddanielasyraf07@gmail.com>
2026-09-11 01:09:24 +0800

    login page created

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+
+    <div class="login-container">
+
+        <h2>Selamat Datang ke Cuna's Bakery</h2>
+
+        <form action="loginact.php" method="POST">
+
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" placeholder="Username" required >
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+            <br>
+            <input type="submit" name="submit" value="Login" class="login-btn">
+
+        </form>
+
+    </div>
+
+</body>
+</html>
