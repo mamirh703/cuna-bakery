a0b72ce860d29d1f9010c7b3f1022204cba62aa1
muhamaddanielasyraf07 <muhamaddanielasyraf07@gmail.com>
2026-09-11 00:45:47 +0800

    Connect update

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
     $servername = "localhost";
-    $userName = "root";
+    $username = "root";
     $password = "";
     $database = "cuna";
 
-    $conn = new mysqli($servername, $userName, $password, $database);
+    $conn = new mysqli($servername, $username, $password, $database);
 
     if ($conn->connect_error){
         die("Connection failed: " . $conn->connect_error);
