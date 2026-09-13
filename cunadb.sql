5698c1209c293cfeb250446a41f307ea712e6482
muhamaddanielasyraf07 <muhamaddanielasyraf07@gmail.com>
2026-09-13 15:35:23 +0800

    Databse

--- cunadb.sql
+++ cunadb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 13, 2026 at 08:26 AM
+-- Generation Time: Sep 13, 2026 at 09:34 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,7 +43,7 @@ CREATE TABLE `orderdetails` (
 
 CREATE TABLE `orders` (
   `orderID` varchar(10) NOT NULL,
-  `userID` varchar(10) NOT NULL,
+  `userID` int(10) NOT NULL,
   `order_date` date NOT NULL,
   `total_amount` decimal(10,2) NOT NULL,
   `order_status` varchar(30) NOT NULL,
@@ -88,13 +88,21 @@ CREATE TABLE `products` (
 --
 
 CREATE TABLE `users` (
-  `userID` varchar(10) NOT NULL,
+  `userID` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `address` varchar(255) DEFAULT NULL,
   `phone` varchar(20) NOT NULL,
   `role` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `password`, `address`, `phone`, `role`) VALUES
+(1, 'Admin', '$2y$10$I/0eX.1xZt4tRtr2mUF.MuvskI9UNZU92myUrDUIBgRmG9txxIoXa', '123, Anywhere, Fake Street', '0123456789', 'admin'),
+(2, 'Daniel', '$2y$10$RFmQornS86gIOflSMra9/eT6RR8Axfp6u0vh0zXCa3WA2V/dUaoQ2', '246, Fake Street, Anywhere', '0544288796', 'user');
 
 --
 -- Indexes for dumped tables
@@ -136,6 +144,16 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `username` (`username`);
 
 --
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -150,7 +168,7 @@ ALTER TABLE `orderdetails`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
 
 --
 -- Constraints for table `payments`
