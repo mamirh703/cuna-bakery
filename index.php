086ea7ce99d066c1fe64164edbb541de8a96d830
Adib Rifqi <muhdadibrifqi@gmail.com>
2026-09-16 19:37:32 +0800

    buat balik untuk improvement dan tukar svg ke shape
    kasi cantik sikit, walaupun svg lagi ok  tapi shape nampak cantik dan responsive ikut saiz screen.

--- index.php
+++ index.php
@@ -12,109 +12,94 @@ $i = 0;
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cuna's Bakery</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=shopping_cart" />
-    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=devimge-width, initial-scale=1.0">
+    <title>test</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&imgon_names=shopping_cart" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatimg.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Chewy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <!-- Navigation -->
+    <!-- Navbar -->
     <nav>
-        <a href="index.php">
-            <h2>CUNA'S BAKERY</h2>
-        </a>
-        <ul>
-            <li><a href="index.php">HOME</a></li>
+        <div class="logo"><a href="#">CUNA'S BAKERY</a></div>
+        <ul class="links">
+            <li><a href="#">HOME</a></li>
             <li><a href="#">ITEMS</a></li>
         </ul>
-        <div class="nav-btn">
-            <div class="order-nav">
-                <button class="order-button">
-                    <span class="order">ORDER NOW</span>
-                </button>
-            </div>
-            <div class="cart-nav">
-                <button class="cart-button">
-                    <span class="material-symbols-outlined">shopping_cart</span>
-                </button>
-            </div>
+        <div class="btns">
+            <a href="" class="order-now-btn">ORDER NOW</a>
+            <a href="" class="cart-btn"><span class="material-symbols-outlined">shopping_cart</span></a>
         </div>
     </nav>
     <!-- Main Content -->
     <main>
-        <!-- info -->
-        <div class="hero">
-            <svg viewbox="-67 -30 450 165">
-                <polygon
-                points="-40,-20 -50,50 -40,125 360,125 370,50 360,-20"
-                fill="white">
-            </svg>
-            <text>
-                CUNA'S BAKERY
-            </text>
-            <p>
-                Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum
-            </p>
-            <div>
-                <button class="order-main">
-                    <a href="#">ORDER NOW</a>
-                </button>
+        <div class="about">
+            <!-- Background shape -->
+            <div class="shape1">
+                <h1>CUNA'S BAKERY</h1>
+                <div class="para">
+                    <p>
+                        Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delimgata usu. Vidit dissentiet eos cu eum
+                    </p><br>
+                    <div class="btns">
+                        <a href="" class="order-btn">ORDER NOW</a>
+                    </div>
+                </div>
             </div>
-            <div class="items-carousel">
-                <div class="ic-cont">
+            <div class="img">
+                <div class="img-cont">
                     <img src="uploads/DSC_0103.JPG">
                 </div>
             </div>
         </div>
-        <!-- Popular item -->
-        <div class="subhero">
-            <svg viewbox="-67 -30 450 215">
-                <polygon
-                    points="160,-15 -50,-5 -50,170 160,180 370,170 370,-5"
-                    fill="white" />
-                </svg>
-                <text x="90" y="15">
-                    POPULAR ITEMS
-                </text>
-            <div class="index-item-col">
-                <?php while ($row = $result->fetch_assoc()) { ?>
-                    <div class="index-item">
-                        <div class="index-item-pic">
-                            <img src="uploads/u good.jpg">
+        <!-- Popular section -->
+        <div class="popular">
+            <!-- background shape 2 -->
+            <div class="shape2">
+                <h1>POPULAR ITEMS</h1>
+                <!-- Parent -->
+                <div class="item-grid">
+                    <?php while ($row = $result->fetch_assoc()) { ?>
+                        <!-- Child -->
+                        <div class="item-card">
+                            <div class="item-card-img">
+                                <img src="uploads/u good.jpg">
+                            </div>
+                            <div class="item-card-details">
+                                <h3><?php echo htmlspecialchars($row['name']) ?></h3>
+                                <p><?php echo htmlspecialchars($row['description']) ?></p>
+                                <p>RM <?php echo htmlspecialchars($row['price']) ?></p>
+                            </div>
+                            <div class="item-card-button">
+                                <button class="item-details <?= $classes[$i] ?>" popovertarget="pd <?= $classes[$i] ?>">DETAILS</button>
+                                <dialog id="pd <?= $classes[$i] ?>" popover>
+                                    <p><?= htmlspecialchars($row['description']) ?></p>
+                                </dialog>
+                                <button><a href='cart.php?table=products&id=".$row["ID"]."'><span class="material-symbols-outlined">shopping_cart</span></a></button>
+                            </div>
                         </div>
-                        <div class="index-item-details">
-                            <h3><?php echo htmlspecialchars($row['name']) ?></h3>
-                            <p><?php echo htmlspecialchars($row['description']) ?></p>
-                            <p>RM <?php echo htmlspecialchars($row['price']) ?></p>
-                        </div>
-                        <div class="index-item-button">
-                            <button class="item-details <?= $classes[$i] ?>" popovertarget="pd <?= $classes[$i] ?>">DETAILS</button>
-                            <dialog id="pd <?= $classes[$i] ?>" popover>
-                                <p><?= htmlspecialchars($row['description']) ?></p>
-                            </dialog>
-                            <a href='cart.php?table=products&id=".$row["ID"]."'><button><span class="material-symbols-outlined">shopping_cart</span></button></a>
-                        </div>
-                    </div>
-                <?php $i++;
-                } ?>
+                    <?php $i++;
+                    } ?>
+                </div>
+                <h6><a href="">VIEW ALL</a></h6>
             </div>
         </div>
     </main>
     <!-- Footer -->
     <footer>
         <div class="brand-footer">
-            <h1>CUNA'S BAKERY</h1>
+            <p>CUNA'S BAKERY</p>
         </div>
         <div class="footer-kanan">
-            <h2>"SWEET AROMA COMES FROM HOMES"</h2>
+            <p>"SWEET AROMA COMES FROM HOMES"</p>
             <div class="footer-kanan-span">
-                <span>cunasbakery</span>
-                <span>cunasbakery</span>
-                <span>012-3456789</span>
+                <span><i class="fab fa-facebook-f"></i> cunasbakery </span>
+                <span><i class="far fa-envelope"></i> cunasbakery</span>
+                <span> <i class="fab fa-whatsapp"></i> 012-3456789</span>
             </div>
         </div>
     </footer>
