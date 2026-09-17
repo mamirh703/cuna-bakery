95b7334d591558fbe692c0c7714344eec558f019
Adib Rifqi <muhdadibrifqi@gmail.com>
2026-09-17 22:34:02 +0800

    dah update full (fix)
    canne boleh terbuang href. hmm

--- admin.php
+++ admin.php
@@ -23,14 +23,14 @@ $result = $conn->query("SELECT * FROM products ORDER BY productID");
 <body>
     <!-- Navbar -->
     <nav>
-        <div class="logo"><a ="index.php">CUNA'S BAKERY</a></div>
+        <div class="logo"><a href="index.php">CUNA'S BAKERY</a></div>
         <ul class="links">
-            <li><a ="index.php">HOME</a></li>
-            <li><a ="items.php">ITEMS</a></li>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="items.php">ITEMS</a></li>
         </ul>
         <div class="btns">
-            <a ="product.php" class="order-now-btn">ORDER NOW</a>
-            <a ="cart.php" class="cart-btn"><span class="material-symbols-outlined">shopping_cart</span></a>
+            <a href="product.php" class="order-now-btn">ORDER NOW</a>
+            <a href="cart.php" class="cart-btn"><span class="material-symbols-outlined">shopping_cart</span></a>
         </div>
     </nav>
     <main>
@@ -66,7 +66,7 @@ $result = $conn->query("SELECT * FROM products ORDER BY productID");
             } ?>
             <!-- Add Item Card -->
             <div class="add-item">
-                <a ="add_item.php">
+                <a href="add_item.php">
                     <svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 -960 960 960" width="100px">
                         <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
                     </svg>
