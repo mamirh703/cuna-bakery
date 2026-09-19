b547a5922816cc0110c2cf07e7983dc2704e1fbb
Adib Rifqi <muhdadibrifqi@gmail.com>
2026-09-19 22:04:06 +0800

    tambah delete button
    Updated HTML comments and added a delete button with confirmation.

--- admin.php
+++ admin.php
@@ -39,8 +39,14 @@ $result = $conn->query("SELECT * FROM products ORDER BY productID");
             <?php while ($row = $result->fetch_assoc()) { ?>
                 <!-- Parent -->
                 <div class="item-card">
-                    <!-- parent -->
+                    <!-- childe -->
                     <div class="item-card-img">
+                        <!-- delete button relative to image -->
+                        <a href="del_item.php?productID=<?= $row['productID'] ?>" onclick="return confirm('Are you sure you want to delete this item?');">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black" style="position: absolute; top: 20; right: -50px; margin: 5px;">
+                                <path d="M280-440h400v-80H280v80ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                            </svg></a>
+                        <!-- gambar -->
                         <img src="uploads/u good.jpg">
                     </div>
                     <div class="item-card-details">
