f4dc3a6b7664d2a694ea1f8c09a1bc743431a1e2
Adib Rifqi <muhdadibrifqi@gmail.com>
2026-09-20 14:07:31 +0800

    Adjust delete button
    Updated the position of the delete button SVG icon in the admin interface.

--- admin.php
+++ admin.php
@@ -43,7 +43,7 @@ $result = $conn->query("SELECT * FROM products ORDER BY productID");
                     <div class="item-card-img">
                         <!-- delete button relative to image -->
                         <a href="del_item.php?productID=<?= $row['productID'] ?>" onclick="return confirm('Are you sure you want to delete this item?');">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black" style="position: absolute; top: 20; right: -50px; margin: 5px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="black" style="position: absolute; top: 30; right: 30px;">
                                 <path d="M280-440h400v-80H280v80ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
                             </svg></a>
                         <!-- gambar -->
