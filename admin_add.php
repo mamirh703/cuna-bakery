01e8ea1a3a22eba7657f2cca812c90926f5688de
Muadri <muhdadibrifqi@gmail.com>
2026-09-21 23:00:26 +0800

    tukar cart ke edit button

--- admin_add.php
+++ admin_add.php
@@ -59,15 +59,15 @@ $result = $conn->query("SELECT * FROM products WHERE is_active = 1 ORDER BY prod
                     <div class="item-card-button">
                         <button class="item-details <?= $row['productID'] ?>" popovertarget="pd<?= $row['productID'] ?>">DETAILS</button>
                         <dialog id="pd<?= $row['productID'] ?>" popover>
-                            <img src="<?= htmlspecialchars($row['image'])?>">
+                            <img src="<?= htmlspecialchars($row['image']) ?>">
                             <p><?= htmlspecialchars($row['description']) ?></p>
                         </dialog>
-                        <form method="POST" action="cart.php" style="display:inline;">
-                            <input type="hidden" name="productID" value="<?= $row['productID'] ?>">
-                            <button class="item-cart" type="submit" name="add_to_cart">
-                                <span class="material-symbols-outlined">shopping_cart</span>
-                            </button>
-                        </form>
+                        <a href="edit_item.php?productID=<?= $row['productID'] ?>">
+                            <button class="item-edit">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="28px" viewBox="0 -960 960 960" width="28px" fill="black">
+                                    <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z" />
+                                </svg></button>
+                        </a>
                     </div>
                 </div>
             <?php
